a80652cc394da4aac89df062dab35a379b489f32
Georgi <georgiivanof@abv.bg>
2015-03-06 00:19:47 +0200

    Update test_preparation.sql

--- 2014-2015/SUBD/test_preparation.sql
+++ 2014-2015/SUBD/test_preparation.sql
@@ -24,7 +24,7 @@ inserts.sql
 insert into Article_18(content, name, published_on) values ('ASD', 'asd', 01/01/2014), ('QWE', 'qwe', 01/01/2014);
 insert into Category(date_created_on, priority) values (01/01/2014, 1), (01/01/2014, 2);
 insert into User(income, picture_url, created_on) values(123, 'www.com', 01/01/2014), (312, 'org.bg', 02/02/2013');
-insert into Tag(name, int) values('pesho', 1), ('gosho'. 2);
+insert into Tag(name, int) values('pesho', 1), ('gosho', 2);
 
 selects1.sql
 SELECT * FROM User;
