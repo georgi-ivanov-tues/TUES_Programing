4cc1d6c007a6f5102b27343f1df331b0c7002d4b
Georgi <georgiivanof@abv.bg>
2015-04-20 14:03:46 +0300

    Update Drawing.c

--- 2014-2015/VMKS/Drawing.c
+++ 2014-2015/VMKS/Drawing.c
@@ -97,42 +97,124 @@ void DrawWinningLine(short x1, short y1, short x2, short y2, Color_t lineColor) 
 	int x_end = 20 + 30*x2;
 	int y_end = 20 + 30*y2;
 
-	int y_begin_offset, y_end_offset;
+	int x_begin_offset, x_end_offset, y_begin_offset, y_end_offset;
 
-	if(y1 < y2) {
+	if(x1 == x2) {
+	 	x_begin_offset = 15;
+		x_end_offset = 15;
+		y_begin_offset = 5;
+		y_end_offset = 25;
+	} else if(y1 < y2) {
+		x_begin_offset = 5;
+		x_end_offset = 25;
 	 	y_begin_offset = 5;
 		y_end_offset = 25;
 	} else if(y1 > y2) {
+		x_begin_offset = 5;
+		x_end_offset = 25;
 	 	y_begin_offset = 25;
 		y_end_offset = 5;
 	} else {
+		x_begin_offset = 5;
+		x_end_offset = 25;
 	 	y_begin_offset = 15;
 		y_end_offset = 15;
 	}
 	
-	LCDSetLine(x_begin + 5, y_begin + y_begin_offset, x_end + 25, y_end + y_end_offset, lineColor);
-	LCDSetLine(x_begin + 4, y_begin + y_begin_offset - 1, x_end + 24, y_end + y_end_offset - 1, lineColor);
-	LCDSetLine(x_begin + 6, y_begin + y_begin_offset + 1, x_end + 26, y_end + y_end_offset + 1, lineColor);					  
+	LCDSetLine(x_begin + x_begin_offset, y_begin + y_begin_offset, x_end + x_end_offset, y_end + y_end_offset, lineColor);
+	if(x1 == x2) {
+		LCDSetLine(x_begin + x_begin_offset + 1 , y_begin + y_begin_offset, x_end + x_end_offset + 1 , y_end + y_end_offset, lineColor);
+		LCDSetLine(x_begin + x_begin_offset - 1, y_begin + y_begin_offset, x_end + x_end_offset - 1 , y_end + y_end_offset, lineColor);
+	} else {
+		LCDSetLine(x_begin + x_begin_offset , y_begin + y_begin_offset - 1, x_end + x_end_offset , y_end + y_end_offset - 1, lineColor);
+		LCDSetLine(x_begin + x_begin_offset , y_begin + y_begin_offset + 1, x_end + x_end_offset , y_end + y_end_offset + 1, lineColor);
+	}					  
 }
+
+// 172.16.15.159:8080
 
 int main()
 {
 	char		msg[100];
 	Color_t		bckgColor=LCD_COLOR_WHITE;
 	Color_t		lineColor=LCD_COLOR_BLACK;
-	int i = 10;
+	int i = 10, current_x, current_y;
+	
+	AT91PS_PIO    p_pPioA  = AT91C_BASE_PIOA;
+	AT91PS_PIO    p_pPioB  = AT91C_BASE_PIOB;
+	AT91PS_PMC    p_pPMC   = AT91C_BASE_PMC;
+	AT91PS_SYS    p_pSys   = AT91C_BASE_SYS;
+
+	AT91F_LowLevel_Init();
+
+	//enable the clock of the PIO
+	p_pPMC->PMC_PCER = 1 << AT91C_ID_PIOA;
+	//enable the clock of the PIO
+	p_pPMC->PMC_PCER = 1 << AT91C_ID_PIOB;
+	
+	// GPIO init
+	p_pPioA->PIO_ODR    = 0xffffffff;   // All as input
+	p_pPioB->PIO_ODR    = 0xffffffff;   // All as input
+	p_pSys->PIOA_PPUDR  = 0xffffffff;   // Disable Pull-up resistor
+	p_pSys->PIOB_PPUDR  = 0xffffffff;   // Disable Pull-up resistor
+
+#define	BIT_SW1 0x01000000
+#define	BIT_SW2 0x02000000
+#define	BIT_SW3 0x03000000
+
+#define	BIT_JUP 0x0000200
+#define	BIT_JDOWN 0x0000100
+#define	BIT_JLEFT 0x000080
+
+#define	BIT_JRIGHT 0x0004000
+#define	BIT_JPUSH 0x00008000
+
+
+	// BUTTON SW1
+	p_pPioB->PIO_ODR |= BIT_SW1; //Configure in Input
+	p_pPioB->PIO_PER |= BIT_SW1; //Enable PB24
+
+	// BUTTON SW2
+	p_pPioB->PIO_ODR |= BIT_SW2; //Configure in Input
+	p_pPioB->PIO_PER |= BIT_SW2; //Enable PB25
+											   
+	// BUTTON SW2
+	p_pPioB->PIO_ODR |= BIT_SW3; //Configure in Input
+	p_pPioB->PIO_PER |= BIT_SW3; //Enable PB25	  PB24
+
+  
+	// JOYSTICK PUSH 
+	p_pPioA->PIO_ODR |= BIT_JPUSH; //Configure in Input
+	p_pPioA->PIO_PER |= BIT_JPUSH; //Enable PA8
+
+	// JOYSTICK LEFT 
+	p_pPioA->PIO_ODR |= BIT_JLEFT; //Configure in Input
+	p_pPioA->PIO_PER |= BIT_JLEFT; //Enable PA8
+	
+	// JOYSTICK RIGHT 
+	p_pPioA->PIO_ODR |= BIT_JRIGHT; //Configure in Input
+	p_pPioA->PIO_PER |= BIT_JRIGHT; //Enable PA8
+	
+	// JOYSTICK UP 
+	p_pPioA->PIO_ODR |= BIT_JUP; //Configure in Input
+	p_pPioA->PIO_PER |= BIT_JUP; //Enable PA8
+
+	// JOYSTICK DWON 
+	p_pPioA->PIO_ODR |= BIT_JDOWN; //Configure in Input
+	p_pPioA->PIO_PER |= BIT_JDOWN; //Enable PA8	
+
+	
 
 	/* any other variables you need ... */
 	// ...
 
 	/* Initialize the Atmel AT91SAM7X256 (watchdog, PLL clock, default interrupts, etc.) */
-	AT91F_LowLevel_Init();
 
 	InitLCD();
 	LCD_ClearScreen( bckgColor );
 
 	DrawGameField(lineColor);
-	DrawIndicator(2, 2, lineColor);
+	//DrawIndicator(2, 2, lineColor);
 	DrawX(1, 1, lineColor);
 	DrawO(1, 2, lineColor);
 	DrawX(0, 0, lineColor);
@@ -142,6 +224,58 @@ int main()
 	/* put other initializations here ... */
 	// ...
 
+
+	current_x = 0;
+	current_y = 0;
+	while ( true ) {	  
+	    // check button SW3
+		
+		/*if((p_pPioA->PIO_PDSR) & BIT_JPUSH){
+			LCD_WriteString("No Push", &Fixedsys_descriptor, 2, 20, LCD_COLOR_WHITE, LCD_COLOR_BLUE);	
+		}
+		else{
+			LCD_WriteString("PUSHEDDDDDD", &Fixedsys_descriptor, 2, 20, LCD_COLOR_WHITE, LCD_COLOR_BLUE);		
+		}*/
+
+		if(((p_pPioA->PIO_PDSR) & BIT_JUP)){
+			
+			//LCD_WriteString("No UP", &Fixedsys_descriptor, 2, 40, LCD_COLOR_WHITE, LCD_COLOR_BLUE);	
+		}
+		else{
+			current_y--;
+			//LCD_WriteString("UPPPPPPPPPP", &Fixedsys_descriptor, 2, 40, LCD_COLOR_WHITE, LCD_COLOR_BLUE);		
+		} 
+
+		if(((p_pPioA->PIO_PDSR) & BIT_JLEFT)){
+			
+			//LCD_WriteString("No LEFT", &Fixedsys_descriptor, 2, 60, LCD_COLOR_WHITE, LCD_COLOR_BLUE);	
+		}
+		else{
+			current_x--;
+			//LCD_WriteString("LEFTTTTTTTT", &Fixedsys_descriptor, 2, 60, LCD_COLOR_WHITE, LCD_COLOR_BLUE);		
+		} 
+
+		if(((p_pPioA->PIO_PDSR) & BIT_JRIGHT)){
+			
+			//LCD_WriteString("No RIGHT", &Fixedsys_descriptor, 2, 80, LCD_COLOR_WHITE, LCD_COLOR_BLUE);	
+		}
+		else{
+			current_x++;
+			//LCD_WriteString("RIGHTTTTTTT", &Fixedsys_descriptor, 2, 80, LCD_COLOR_WHITE, LCD_COLOR_BLUE);		
+		} 
+
+		if((p_pPioA->PIO_PDSR) & BIT_JDOWN){
+			
+			//LCD_WriteString("No DOWN", &Fixedsys_descriptor, 2, 100, LCD_COLOR_WHITE, LCD_COLOR_BLUE);	
+		}
+		else{
+			current_y++;
+			//LCD_WriteString("DOWNNNNNNNN", &Fixedsys_descriptor, 2, 100, LCD_COLOR_WHITE, LCD_COLOR_BLUE);		
+		} 
+
+		DrawIndicator(current_x, current_y, lineColor);
+		Delay(1000000);
+	}
 	/* enable interrupts */
 	//AT91F_Finalize_Init();
 
